21e01773d7ee586b6589e6074230f1a20e4647ea
Sabina Buruiana <sabina_buruiana@yahoo.com>
2017-03-01 19:37:45 +0200

    Sixth commit
    Push from collectionViewCell to tableView controller with static cells

--- Users/TableViewController.swift
+++ Users/TableViewController.swift
@@ -15,22 +15,24 @@ class TableViewController: UITableViewController {
     
     
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var userImageView: RoundFullCircleView!
     @IBOutlet weak var jobLabel: UILabel!
     @IBOutlet weak var usernameLabel: UILabel!
-    @IBOutlet weak var userImageView: RoundFullCircleView!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var phoneLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        nameLabel.text = (person?.firstName)! + (person?.lastName)!
+        nameLabel.text = (person?.firstName)! + " " + (person?.lastName)!
         phoneLabel.text = person?.phone
         emailLabel.text = person?.email
-        addressLabel.text = (person?.city)! + (person?.street)!
+        addressLabel.text = (person?.city)! + " " + (person?.street)!
         jobLabel.text = "HR"
         usernameLabel.text = person?.username
-        userImageView.sd_setImage(with:URL(string:(person?.mediumURL)!))
+        if let picture = person?.mediumURL {
+           userImageView.sd_setImage(with:URL(string:picture))
+        }
 
     }
 
